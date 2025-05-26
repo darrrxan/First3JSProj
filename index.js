2b2ad8b03a51d31657eb6145b4000d09fd7815fe
darrrxan <darshanmahanubhav9@gmail.com>
2025-05-26 19:57:24 +0530

    This is just for fun

---
+++ index.js
@@ -0,0 +1,8 @@
+// JavaScript program to illustrate If statement
+let i = 10;
+
+if (i > 15) console.log("10 is less than 15");
+
+// This statement will be executed
+// as if considers one statement by default
+console.log("I am Not in if");
